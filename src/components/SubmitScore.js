cb917bfb6213ecfcfdcee64892229a4fb1800797
Joseph Graham <Joseph@Chasingthefiks.net>
2020-01-09 21:39:09 -0700

    redirect after submit

--- src/components/SubmitScore.js
+++ src/components/SubmitScore.js
@@ -22,6 +22,7 @@ class SubmitScore extends Component {
       })
       .then(res => console.log(res))
       .catch(err => console.log(err));
+    this.props.history.push("/QuizSelector");
   }
   render() {
     return (
